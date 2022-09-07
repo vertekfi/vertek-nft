2cf3b6c6e6bf7b9eee354d42b2d246f0ea30a007
0xBriz <chaindevone@gmail.com>
2022-09-07 07:39:09 -0400

    clean up

---
+++ test/spec.ts
@@ -0,0 +1,12 @@
+import { time, loadFixture } from "@nomicfoundation/hardhat-network-helpers";
+import { anyValue } from "@nomicfoundation/hardhat-chai-matchers/withArgs";
+import { expect } from "chai";
+import { ethers } from "hardhat";
+
+describe("Tests", () => {
+  describe("Deployment", () => {
+    it("Should", async function () {
+      expect(true).to.be.true;
+    });
+  });
+});
