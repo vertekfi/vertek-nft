686343ac976c25392ce45847d9332d6ff914749a
0xBriz <chaindevone@gmail.com>
2023-01-12 14:39:50 -0500

    test fixture placeholder

--- test/spec.ts
+++ test/spec.ts
@@ -1,6 +1,7 @@
-import * as helpers from "@nomicfoundation/hardhat-network-helpers";
+import { loadFixture } from "@nomicfoundation/hardhat-network-helpers";
 import { expect } from "chai";
 import { ethers } from "hardhat";
+import { awesomeFixture } from "./fixtures/awesome.fixture";
 
 describe("Tests", () => {
   beforeEach(async () => {
@@ -8,6 +9,7 @@ describe("Tests", () => {
   });
 
   it("Should", async () => {
+    const fixture = await loadFixture(awesomeFixture)
     expect(true).to.be.true;
   });
 });
