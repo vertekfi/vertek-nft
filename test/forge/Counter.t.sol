cf8af9c1927757d2aa54bca430a41c3743df3c73
0xBriz <chaindevone@gmail.com>
2023-02-14 12:11:12 -0500

    fixed forge test imports

--- test/forge/Counter.t.sol
+++ test/forge/Counter.t.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
+pragma solidity ^0.8.12;
 
 import "forge-std/Test.sol";
-import "../src/Counter.sol";
+import "../../contracts/test/Counter.sol";
 
 contract CounterTest is Test {
     Counter public counter;
